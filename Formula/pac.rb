8777444ba8abf074dac91dbabbff69708700783a
WestleyK <westley@sylabs.io>
2019-11-05 15:21:33 -0800

    Added pac formula

---
+++ Formula/pac.rb
@@ -0,0 +1,21 @@
+class ListFiles < Formula
+  desc "print some files"
+  homepage ""
+  url "https://github.com/WestleyR/pac/archive/v1.0.0.tar.gz"
+  sha256 "1b8e6857c2627396c4dc5a965c909a656bd2b1b9ef1a841ddcad1dc48abbec79"
+
+  bottle do
+    cellar :any
+    rebuild 1
+  end
+
+  head do
+    url "https://github.com/WestleyR/pac.git"
+    depends_on "libtool" => :build
+    depends_on "gcc" => :build
+  end
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+end
