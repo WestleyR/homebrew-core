ac805ddd245cc3b7fa8821364e38b4772c28d844
WestleyK <westley@sylabs.io>
2019-11-05 14:37:24 -0800

    Bumped version (test)

--- Formula/list-files.rb
+++ Formula/list-files.rb
@@ -1,16 +1,12 @@
 class ListFiles < Formula
   desc "list some files"
   homepage ""
-  url "https://github.com/WestleyR/list-files/archive/v1.0.1.tar.gz"
-  sha256 "f3d38a4eb265ebb2323a64eb82cbb98a4ea67613d0f6df69a1d9d2a6f76f683f"
-
-  #url "https://github.com/WestleyR/list-files/archive/v1.0.0.tar.gz"
-  #sha256 "ee183ef9c5aedf5374053855637ff27f9c66cabded18cf87c88a06bbaf01e28c"
+  url "https://github.com/WestleyR/list-files/archive/v1.0.2.tar.gz"
+  sha256 "cdea8a86da5f2c37efa210ed63547aa169151cec277ed8f0604dccf374c972a2"
 
   bottle do
     cellar :any
     rebuild 1
-#    sha256 "null" => :catalina
   end
 
   head do
