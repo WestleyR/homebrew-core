3ccae449ab0b32b2a26ea949619410c881545f3d
WestleyK <westley@sylabs.io>
2019-11-05 17:49:02 -0800

    Updated tac

--- Formula/tac.rb
+++ Formula/tac.rb
@@ -1,8 +1,8 @@
 class Tac < Formula
   desc "tac command for macOS (linux too)"
   homepage "https://github.com/WestleyR/tac"
-  url "https://github.com/WestleyR/tac/archive/tac-1.0.0.tar.gz"
-  sha256 "3432c365f721ec1285e0dfaf6f3fb2eed8fb2470aa364f1fb6a65d0b23228733"
+  url "https://github.com/WestleyR/tac/archive/v1.0.1.tar.gz"
+  sha256 "e2458464709a0b027bb7834fe6362fb0ee25ac9bbe938050c1d9fbe0d9846387"
 
   # depends_on "cmake" => :build
 
