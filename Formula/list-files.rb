e5b3e89ea68af83e247b9cd802ecf93644638354
WestleyK <westleyk@nym.hush.com>
2020-08-25 19:49:18 -0700

    Updated very old package

--- Formula/list-files.rb
+++ Formula/list-files.rb
@@ -1,8 +1,8 @@
 class ListFiles < Formula
   desc "list some files"
   homepage ""
-  url "https://github.com/WestleyR/list-files/archive/v1.5.3.tar.gz"
-  sha256 "3af24b15934dc0c235bf6589958fe2c70b2746bc1d921c68701a9d8cdc395d39"
+  url "https://github.com/WestleyR/list-files/archive/v1.6.2.tar.gz"
+  sha256 "56e6ab605cea0205a3bb061ccb15c71b0764b31983c2509b746637ea1bd7dee0"
 
   bottle do
     cellar :any
